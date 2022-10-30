d16d43bfd184b3ae2b13df670f3e51bf7babaee7
bwaobikeze <bwaobikeze@hotmail.com>
2022-10-29 22:37:03 -0500

    added delete function

--- src/main/java/org/example/avlTree.java
+++ src/main/java/org/example/avlTree.java
@@ -73,6 +73,65 @@ public class avlTree {
         }
     return Node;
     }
+    node deletingNode(node root, int value){
+        if (root==null){
+            return root;
+        }
+        if(value<root.value){
+            root.left=deletingNode(root.left,value);
+        }
+        else if(value> root.value){
+            root.right=deletingNode(root.right,value);
+        }
+        else{
+            if(root.left==null|| root.right==null){
+                node temp=null;
+                if(temp==root.left){
+                    temp=root.right;
+                }
+                else{
+                    temp=root.left;
+                    if(temp==null){
+                        temp=root;
+                        root=null;
+                    }
+                        else{
+                            root=temp;
+                        }
+                }
+            }
+            else{
+                node temp=FindMinVal(root.right);
+                root.value=temp.value;
+                root.right=deletingNode(root.right, temp.value);
+            }
+        }
+        //update balance
+        if(root==null){
+            return root;
+        }
+        root.height=findMax(findingHeight(root.left),findingHeight(root.right))+1;
+        int balanceFact=restrieveBalnceFact(root);
+        if(balanceFact>1){
+            if(restrieveBalnceFact(root.left)>=0){
+                return rotateRight(root);
+            }
+            else{
+                root.left=rotateLeft(root.left);
+                return rotateRight(root);
+            }
+        }
+        if(balanceFact<-1){
+            if(restrieveBalnceFact(root.right)<=0){
+                return rotateLeft(root);
+            }
+            else{
+                root.right=rotateRight(root.right);
+                return rotateLeft(root);
+            }
+        }
+        return root;
+    }
 node FindMinVal(node val){
         node current=val;
         while(current.left!=null){
